220841bc6fb29edb60be2f6fea29b4c66accf520
Stephane Briclot <stephane.briclot@gmail.com>
2021-02-01 18:59:47 +0100

    Add model tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
-class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class UserTest < ActiveSupport::TestCase
+  test "full product entry (name + tagline)" do
+    product = Product.new(name: "skello", tagline: "planning tool")
+    assert_equal true, product.valid?
+  end
+
+  test "name only" do
+    product = Product.new(name: "skello")
+    assert_equal false, product.valid?
+  end
+
+  test "tagline only" do
+    product = Product.new(tagline: "planning tool")
+    assert_equal false, product.valid?
+  end
 end
